23387738709e154ce197ff33e9c586f364f8a2ce
Abhishek <abhimore74@gmail.com>
2022-12-18 13:09:41 -0500

    banner

---
+++ components/common/Banner.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Banner() {
+  return (
+    <a href="https://tamuhack.com/" target="_blank" rel="noopener noreferrer" >
+      <div className="absolute flex justify-center items-center py-6 px-8 h-8 top-0 w-screen bg-blue-500 ">
+        <p className="text-white font-bold">Register for TAMUhack \\ January 28-29</p>
+      </div>
+    </a>
+  )
+}
