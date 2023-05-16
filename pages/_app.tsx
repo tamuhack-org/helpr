5772084325cf595047f34909f0bc4adef02f267a
Abhishek More <abhimore74@gmail.com>
2023-05-15 23:43:28 -0500

    added vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
 import { ChakraProvider } from '@chakra-ui/react';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
       <SessionProvider session={pageProps.session}>
         <Component {...pageProps} />
+        <Analytics />
       </SessionProvider>
     </ChakraProvider>
   );
