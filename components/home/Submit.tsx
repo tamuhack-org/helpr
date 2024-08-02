9933eaa136461128534cb5232fb0f97144d3351e
Abhishek More <abhimore74@gmail.com>
2024-08-02 14:17:00 -0400

    form functionality

--- components/home/Submit.tsx
+++ components/home/Submit.tsx
@@ -7,42 +7,43 @@ import { fetcher } from '../../lib/common';
 import { useToast } from '@chakra-ui/react';
 import { useState } from 'react';
 import { useSWRConfig } from 'swr';
+import { useForm, SubmitHandler } from 'react-hook-form';
+
+//Using React Hook Form for for submission
+//Just using the built in validation for this simple case, but look into Zod or Yup for more complex validation
+
+type Inputs = {
+  issue: string;
+  location: string;
+  contact: string;
+};
 
 export default function Submit() {
   const toast = useToast();
-
-  const { data, error, isLoading } = useSWR('/api/users/me', fetcher, {
-    refreshInterval: 1000,
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<Inputs>({
+    resetOptions: { keepValues: true },
   });
 
-  const { mutate } = useSWRConfig();
-  const [submitLoading, setSubmitLoading] = useState(false);
-  const [issue, setIssue] = useState('');
-  const [location, setLocation] = useState('');
-  const [contact, setContact] = useState('');
-
-  if (isLoading || error) {
-    return <p>Loading</p>;
-  }
-
-  async function submitTicket() {
-    if (submitLoading) {
-      return;
-    }
+  const onSubmit: SubmitHandler<Inputs> = async (
+    data,
+    e?: React.BaseSyntheticEvent
+  ) => {
+    e?.preventDefault();
 
     try {
       setSubmitLoading(true);
-      if (!issue || !location || !contact) {
-        throw new Error('Please fill out all fields');
-      }
-
       await axios({
         method: 'post',
         url: '/api/tickets/create',
         data: {
-          issue: issue,
-          location: location,
-          contact: contact,
+          issue: data.issue,
+          location: data.location,
+          contact: data.contact,
         },
       }).then(async function () {
         await mutate('/api/users/me');
@@ -67,6 +68,17 @@ export default function Submit() {
     } finally {
       setSubmitLoading(false);
     }
+  };
+
+  const { data, error, isLoading } = useSWR('/api/users/me', fetcher, {
+    refreshInterval: 1000,
+  });
+
+  const { mutate } = useSWRConfig();
+  const [submitLoading, setSubmitLoading] = useState(false);
+
+  if (isLoading || error) {
+    return <p>Loading</p>;
   }
 
   async function cancelTicket() {
@@ -84,9 +96,10 @@ export default function Submit() {
       }).then(async function () {
         await mutate('/api/users/me');
       });
+
+      reset();
     } catch (e) {
       const error = e as Error;
-      console.log(error.message);
       toast({
         title: 'Error',
         description: error.message,
@@ -127,45 +140,52 @@ export default function Submit() {
         <p className="font-bold text-3xl text-gray-700">Submit ticket</p>
         <InfoModal email={data.user.email} />
       </div>
-      <p className="mt-3 text-md text-gray-600">Issue</p>
-      <Input
-        id="issue"
-        className="mt-2"
-        variant="outline"
-        placeholder="Issue"
-        value={issue}
-        onChange={(e) => setIssue(e.target.value)}
-      />
-      <p className="mt-3 text-md text-gray-600">
-        Location (so we can find you!)
-      </p>
-      <Input
-        id="location"
-        className="mt-2"
-        variant="outline"
-        placeholder="Location"
-        value={location}
-        onChange={(e) => setLocation(e.target.value)}
-      />
-      <p className="mt-3 text-md text-gray-600">
-        Contact (if we can&apos;t find you!)
-      </p>
-      <Input
-        id="contact"
-        className="mt-2"
-        variant="outline"
-        placeholder="Contact"
-        value={contact}
-        onChange={(e) => setContact(e.target.value)}
-      />
-      <button
-        onClick={() => submitTicket()}
-        className={`${
-          submitLoading ? 'bg-gray-500' : 'bg-blue-500'
-        } w-full mt-8 py-4 px-8 text-white font-bold rounded-xl`}
+      <form
+        className="flex flex-col gap-3 mt-3"
+        onSubmit={handleSubmit(onSubmit)}
       >
-        {submitLoading ? 'Submitting Ticket' : 'Submit Ticket'}
-      </button>
+        <div className="flex flex-col gap-2">
+          <p className="text-md text-gray-600">Issue</p>
+          <Input
+            variant="outline"
+            placeholder="Issue"
+            errorBorderColor="crimson"
+            {...register('issue', { required: true, maxLength: 20 })}
+            isInvalid={errors.issue ? true : false}
+          />
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <p className="text-md text-gray-600">
+            Location (so we can find you!)
+          </p>
+          <Input
+            variant="outline"
+            placeholder="Location"
+            {...register('location')}
+          />
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <p className="text-md text-gray-600">
+            Contact (if we can&apos;t find you!)
+          </p>
+          <Input
+            variant="outline"
+            placeholder="Contact"
+            {...register('contact')}
+          />
+        </div>
+
+        <button
+          type="submit"
+          className={`${
+            submitLoading ? 'bg-gray-500' : 'bg-blue-500'
+          } w-full mt-8 py-4 px-8 text-white font-bold rounded-xl`}
+        >
+          {submitLoading ? 'Submitting Ticket' : 'Submit Ticket'}
+        </button>
+      </form>
     </div>
   );
 }
