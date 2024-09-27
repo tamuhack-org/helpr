0fd6766560c3e49f53d4394b870ee2814d2f5219
Abhishek <abhimore74@gmail.com>
2024-09-26 19:19:33 -0500

    extend issue length

--- lib/common.ts
+++ lib/common.ts
@@ -21,6 +21,6 @@ export const getTimeDifferenceString = (time: Date) => {
 export const phoneNumberRegex =
   /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
 
-export const maxIssueLength = 80;
+export const maxIssueLength = 120;
 export const maxLocationLength = 60;
 export const maxPhoneLength = 20;
